fcc04ca0ca57b8dd7681ec8ed3aea4669e341323
Md.Amanullah Akon <79790568+amandiu@users.noreply.github.com>
2024-06-20 08:15:24 +0600

    first commit

---
+++ OpenSea/src/App.jsx
@@ -0,0 +1,16 @@
+import Catagories from "./Components/Catagories";
+import Header from "./Components/Header";
+import Cart from "./Components/catagoriesCart"
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Catagories/>
+      <Cart/>
+      <h2>Hellow Open Sea...!!</h2>
+    </div>
+  );
+};
+
+export default App;
