69283024341857b3dcc3edd9d352e5565d92b0bf
Md.Amanullah Akon <79790568+amandiu@users.noreply.github.com>
2024-06-20 22:42:58 +0600

    Ranking section update now

--- OpenSea/src/App.jsx
+++ OpenSea/src/App.jsx
@@ -1,16 +1,18 @@
 import Catagories from "./Components/Catagories";
 import Header from "./Components/Header";
+import Ranking from "./Components/Ranking";
 import Trending from "./Components/Trending";
-import Cart from "./Components/catagoriesCart"
+import Cart from "./Components/catagoriesCart";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Catagories/>
-      <Cart/>
-      <Trending/>
-      <h2>Hellow Open Sea...!!</h2>
+      <Catagories />
+      <Cart />
+      <Trending />
+      <Ranking />
+      <h2>Hello Open Sea...!!</h2>
     </div>
   );
 };
